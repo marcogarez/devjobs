a1686a86ac778e14948cdadbc96a26d9b45da17d
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-10 10:59:41 -0500

    Publicacion de paquetes de laravel desde artisan. Fetch de vacantes desde la clase de livewire. Mostrando las vacantes en caso de que existan haciendo uso de forelse. Propiedad dates añadida a model de vacante para poder formatear datos con format().

---
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -0,0 +1,28 @@
+<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    @forelse ($vacantes as $vacante)
+        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+            <div class="space-y-3">
+                <a href="#" class="textxl font-bold">
+                    {{ $vacante->titulo }}
+                </a>
+                <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+                <p class="text s- text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+            </div>
+
+            <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+                <a href="#"
+                    class="bg-slate-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase text-center">Candidatos</a>
+                <a href="#"
+                    class="bg-blue-800 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase text-center">Editar</a>
+                <a href="#"
+                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-sx font-bold uppercase text-center">Eliminar</a>
+            </div>
+        </div>
+    @empty
+        <p class="p-10 text-center text-sm text-gray-600">No hay vacantes para mostrar</p>
+    @endforelse
+</div>
+
+<div class="mt-10">
+    {{ $vacantes->links() }}
+</div>
--- resources/views/vacantes/index.blade.php
+++ resources/views/vacantes/index.blade.php
@@ -14,11 +14,7 @@
                 </div>
             @endif
             
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Vacantes
-                </div>
-            </div>
+            <livewire:mostrar-vacantes />
         </div>
     </div>
 </x-app-layout>
