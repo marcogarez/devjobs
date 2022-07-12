1b9991af2fd8e22066390619a0e324145c3323aa
Marco Garcia <dev.84ui7@aleeas.com>
2022-07-12 16:48:44 -0500

    Livewire component to upload dev documents added

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <p>Im here</p>
+<div class="bg-gray-100 p-5 mt-10 flex justify-center flex-col items-center">
+    <h3 class="text-center text-2xl font-bold my-4">Postularme a esta vacante</h3>
+
+    <form action="" class="w-96 mt-5">
+        <div class="mb-4">
+            <x-label for="cv" :value="__('Curriculum u hoja de vida (PDF)')" />
+            <x-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+        </div>
+    </form>
 </div>
