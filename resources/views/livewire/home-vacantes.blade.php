0b4e4b8651c40dac26e09851f33483a8d2323c85
marcogarez <dev.84ui7@aleeas.com>
2022-07-16 00:36:18 -0500

    Search engine implemented

---
+++ resources/views/livewire/home-vacantes.blade.php
@@ -0,0 +1,31 @@
+<div>
+
+    @livewire('filtrar-vacantes')
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto">
+            <h3 class="font-extrabold text-4xl text-gray-800 mb-12">Vacantes disponibles</h3>
+
+            <div class="bg-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
+                @forelse ($vacantes as $vacante)
+                    <div class="md:flex md:justify-between md:items-center py-5">
+                        <div class="md:flex-1">
+                            <a href="{{ route('vacantes.show', $vacante->id) }}" class="text-3xl font-extrabold">{{ $vacante->titulo }}</a>
+                            <p class="text-base text-gray-600 mb-1">{{ $vacante->empresa }}</p>
+                            <p class="text-base text-gray-600 mb-1">{{ $vacante->categoria->categoria }}</p>
+                            <p class="text-base text-gray-600 mb-1">{{ $vacante->salario->salario }}</p>
+                            <p class="font-bold text-xs text-gray-600">Último día poara postularse:
+                                <span class="font-normal">{{ $vacante->ultimo_dia->format('d/m/y') }}</span>
+                            </p>
+                        </div>
+                        <div class="mt-5 md:mt-0">
+                            <a class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white rounded-lg block text-center" href="{{ route('vacantes.show', $vacante->id) }}">Ver vacante</a>
+                        </div>
+                    </div>
+                @empty
+                    <p class="p-3 text-center text-sm text-gray-600">Aún no hay vacantes</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+</div>
