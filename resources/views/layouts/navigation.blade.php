ff5ce5a4acd479dc048d4460d73fece42876a183
marcogarez <dev.84ui7@aleeas.com>
2022-07-15 22:01:52 -0500

    Notifications screen added. Notifications functionallity added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,14 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @auth
+
+                    @if (auth()->user()->rol === 2)
+                        <a class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white"
+                            href="{{ route('notificaciones') }}">
+                            {{ auth()->user()->unreadNotifications->count() }}
+                        </a>
+                    @endif
+
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
@@ -96,6 +104,18 @@
                 <x-responsive-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
                     {{ __('Crear vacante') }}
                 </x-responsive-nav-link>
+
+                @if (auth()->user()->rol === 2)
+                    <div class="flex gap-2 items-center p-3">
+                        <a class="w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-sm font-extrabold text-white"
+                            href="{{ route('notificaciones') }}">
+                            {{ auth()->user()->unreadNotifications->count() }}
+                        </a>
+                        <p class="text-base font-medium text-gray-600">
+                            Notificaciones
+                        </p>
+                    </div>
+                @endif
             </div>
 
             <!-- Responsive Settings Options -->
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\VacanteController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,5 +24,7 @@ Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware
 
 Route::get('/vacantes/{vacante}/edit', [VacanteController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacantes.edit');
 Route::get('/vacantes/{vacante}', [VacanteController::class, 'show'])->name('vacantes.show');
+
+Route::get('/notificaciones', NotificacionController::class)->name('notificaciones');
 
 require __DIR__.'/auth.php';
